ea5c848882755fac53d591cbed3e501248691f03
sonu-A <itssonu571@gmail.com>
2026-06-20 12:49:25 +0530

    did 2 sums in js and those were intresting

--- jspractice.js
+++ jspractice.js
@@ -254,9 +254,104 @@
 // console.log(kj)
 
 
-const r=[20,253333,350]
-r.sort((a,b)=> a-b)
-console.log(r)
+// const r=[20,253333,350]
+// r.sort((a,b)=> a-b)
+// console.log(r)
+
+
+// const students = [
+//   { rollNo: 101, name: "Arun", department: "CSE", marks: 85 },
+//   { rollNo: 102, name: "Ravi", department: "CSD", marks: 22 },
+//   { rollNo: 103, name: "Sita", department: "CSM", marks: 78 },
+//   { rollNo: 104, name: "Kiran", department: "IT", marks: 45 },
+//   { rollNo: 105, name: "Priya", department: "CSO", marks: 91 },
+
+//   { rollNo: 106, name: "Rahul", department: "CSE", marks: 30 },
+//   { rollNo: 107, name: "Sneha", department: "CSD", marks: 67 },
+//   { rollNo: 108, name: "Teja", department: "CSM", marks: 55 },
+//   { rollNo: 109, name: "Divya", department: "IT", marks: 29 },
+//   { rollNo: 110, name: "Ajay", department: "CSO", marks: 72 },
+
+//   { rollNo: 111, name: "Anil", department: "CSE", marks: 95 },
+//   { rollNo: 112, name: "Meena", department: "CSD", marks: 41 },
+//   { rollNo: 113, name: "Vamsi", department: "CSM", marks: 18 },
+//   { rollNo: 114, name: "Nisha", department: "IT", marks: 88 },
+//   { rollNo: 115, name: "Kavya", department: "CSO", marks: 36 },
+
+//   { rollNo: 116, name: "Ramesh", department: "CSE", marks: 50 },
+//   { rollNo: 117, name: "Pooja", department: "CSD", marks: 93 },
+//   { rollNo: 118, name: "Tarun", department: "CSM", marks: 34 },
+//   { rollNo: 119, name: "Deepika", department: "IT", marks: 62 },
+//   { rollNo: 120, name: "Manoj", department: "CSO", marks: 28 },
+
+//   { rollNo: 121, name: "Lokesh", department: "CSE", marks: 74 },
+//   { rollNo: 122, name: "Anusha", department: "CSD", marks: 81 },
+//   { rollNo: 123, name: "Sai", department: "CSM", marks: 39 },
+//   { rollNo: 124, name: "Harika", department: "IT", marks: 27 },
+//   { rollNo: 125, name: "Keerthi", department: "CSO", marks: 58 },
+
+//   { rollNo: 126, name: "Bhanu", department: "CSE", marks: 33 },
+//   { rollNo: 127, name: "Mahesh", department: "CSD", marks: 46 },
+//   { rollNo: 128, name: "Jyothi", department: "CSM", marks: 87 },
+//   { rollNo: 129, name: "Chandu", department: "IT", marks: 53 },
+//   { rollNo: 130, name: "Suresh", department: "CSO", marks: 20 }
+// ];
+
+
+
+// const passed=students.filter(a=>a.marks>=35)
+// console.log(`passed students are : ${passed.name}`)
+
+// const cse_dp=students.filter(b=>b.department=="CSE")
+// console.log(cse_dp)
+
+// const csd_dp=students.filter(b=>b.department=="CSD")
+// console.log(csd_dp)
+
+// const csm_dp=students.filter(b=>b.department=="CSM")
+// console.log(csm_dp)
+
+
+
+const students = [
+  { rollNo: 101, name: "Arun", department: "CSE", marks: 85, placed: true, package: 12 },
+  { rollNo: 102, name: "Ravi", department: "IT", marks: 65, placed: false, package: 0 },
+  { rollNo: 103, name: "Sita", department: "CSD", marks: 92, placed: true, package: 15 },
+  { rollNo: 104, name: "Rahul", department: "CSM", marks: 55, placed: true, package: 6 },
+  { rollNo: 105, name: "Priya", department: "CSO", marks: 88, placed: true, package: 18 },
+  { rollNo: 106, name: "Ajay", department: "CSE", marks: 72, placed: false, package: 0 },
+  { rollNo: 107, name: "Sneha", department: "IT", marks: 81, placed: true, package: 10 },
+  { rollNo: 108, name: "Kiran", department: "CSD", marks: 49, placed: false, package: 0 },
+  { rollNo: 109, name: "Deepika", department: "CSM", marks: 90, placed: true, package: 14 },
+  { rollNo: 110, name: "Manoj", department: "CSO", marks: 76, placed: true, package: 8 }
+];
+
+
+const placed_s= students.filter(a=>a.placed==true)
+console.log(placed_s)
+
+const names_packages= students.map(a=>((a.name,a.package)))
+console.log(names_packages)
+
+const packages= students.map((a=>a.package))
+console.log(packages)
+
+
+const ave=packages.reduce((a,b)=>(a+b),0)
+console.log(ave)
+
+const  sortt=packages.sort((a,b)=>a-b)
+console.log(packages)
+
+const  dortt=packages.sort((a,b)=>a+b)
+console.log(dortt[0])
+
+o=placed_s.filter(a=>a.package>10)
+console.log(o)
+
+n=o.map(a=>a.name)
+console.log(n)
+
 
 
 
